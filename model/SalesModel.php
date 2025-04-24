e0c4eac15615c6dc1dd8b0bdad0044c18332007a
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2025-04-24 15:14:46 -0600

    Reunion

--- model/SalesModel.php
+++ model/SalesModel.php
@@ -1,32 +1,37 @@
-<?php 
+<?php
 
-require_once '../core/Model.php';
+require_once './core/Model.php';
 
-class SalesModel extends Model {
-    public function get($id = null) {
-        if ($id) {
-            $query = "SELECT * FROM sales WHERE id = :id";
-            return $this->getQuery($query, [':id' => $id]);
-        } else {
-            $query = "SELECT * FROM sales";
-            return $this->getQuery($query);
-        }
+class SalesModel extends Model
+{
+  public function get($id = null)
+  {
+    if ($id) {
+      $query = "SELECT * FROM sales WHERE id = :id";
+      return $this->getQuery($query, [':id' => $id]);
+    } else {
+      $query = "SELECT * FROM sales";
+      return $this->getQuery($query);
     }
+  }
 
-    public function add($data) {
-        $query = "INSERT INTO sales (user_id, total, date, state) VALUES (:user_id, :total, :date, :state)";
-        return $this->setQuery($query, $data);
-    }
+  public function add($data)
+  {
+    $query = "INSERT INTO sales (user_id, total, date, state) VALUES (:user_id, :total, :date, :state)";
+    return $this->setQuery($query, $data);
+  }
 
-    public function update($data) {
-        $query = "UPDATE sales SET user_id = :user_id, total = :total, date = :date, state = :state WHERE id = :id";
-        return $this->setQuery($query, $data);
-    }
+  public function update($data)
+  {
+    $query = "UPDATE sales SET user_id = :user_id, total = :total, date = :date, state = :state WHERE id = :id";
+    return $this->setQuery($query, $data);
+  }
 
-    public function delete($id) {
-        $query = "UPDATE FROM sales set state = :state  WHERE id = :id";
-        return $this->setQuery($query, [':id' => $id, ':state' => 0]);
-    }
+  public function delete($id)
+  {
+    $query = "UPDATE FROM sales set state = :state  WHERE id = :id";
+    return $this->setQuery($query, [':id' => $id, ':state' => 0]);
+  }
 }
 
 
--- model/UsersModel.php
+++ model/UsersModel.php
@@ -1,44 +1,44 @@
-<?php 
+<?php
 
 require_once 'core/Model.php';
 require_once 'core/Validator.php';
 
 class UsersModel extends Model
 {
-    public function getUsers($id=null)
-    {
-        if ($id) {
-            $query = "SELECT * FROM users";
-            return $this->getQuery($query);
-        } else {    
-            $query = "SELECT * FROM users WHERE id = :id";
-            return $this->getQuery($query, [':id' => $id]);
-        }
+  public function getUsers($id = null)
+  {
+    if ($id) {
+      $query = "SELECT * FROM users";
+      return $this->getQuery($query);
+    } else {
+      $query = "SELECT * FROM users WHERE id = :id";
+      return $this->getQuery($query, [':id' => $id]);
     }
+  }
 
-    public function login($email, $password)
-    {
-        $query = "SELECT id_role, username, role FROM users INNER JOIN roles on users.role_id = roles.role_id WHERE email = :email AND password = SHA2(:password,256)";
-        return $this->getQuery($query, [':email' => $email, ':password' => $password]);
-    }
+  public function login($email, $password)
+  {
+    $query = "SELECT id_role, username, role FROM users INNER JOIN roles on users.id_role = roles.role_id WHERE email = :email AND password = :password";
+    return $this->getQuery($query, [':email' => $email, ':password' => $password]);
+  }
 
-    public function create($user)
-    {
-        $query = "INSERT INTO users (username, email, password, phone, id_role, state) VALUES (:username, :email, :password, :phone, :id_role, :state)";
-        return $this->setQuery($query, $user);
-    }
+  public function create($user)
+  {
+    $query = "INSERT INTO users (username, email, password, phone, id_role, state) VALUES (:username, :email, :password, :phone, :id_role, :state)";
+    return $this->setQuery($query, $user);
+  }
 
-    public function update($user)
-    {
-        $query = "UPDATE users SET username = :username, email = :email, phone = :phone, id_role = :id_role, state = :state WHERE id = :id";
-        return $this->setQuery($query, $user);
-    }
+  public function update($user)
+  {
+    $query = "UPDATE users SET username = :username, email = :email, phone = :phone, id_role = :id_role, state = :state WHERE id = :id";
+    return $this->setQuery($query, $user);
+  }
 
-    public function delete($id)
-    {
-        $query = "DELETE FROM users WHERE id = :id";
-        return $this->setQuery($query, [':id' => $id]);
-    }
+  public function delete($id)
+  {
+    $query = "DELETE FROM users WHERE id = :id";
+    return $this->setQuery($query, [':id' => $id]);
+  }
 
 }
 ?>
